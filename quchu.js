006c973227a4569567ce7498e520915ad7f98c34
shini74744 <158538512+shini74744@users.noreply.github.com>
2025-09-12 16:59:24 +0800

    Update quchu.js

--- quchu.js
+++ quchu.js
@@ -1,9 +1,9 @@
-(function () {
-  // 只在 shli.io 域名下执行
+// 去除顶部显示按钮，仅在 shli.io 下执行
+(function() {
   if (window.location.hostname !== 'shli.io') return;
-// 去除顶部显示按钮
 
-['radix-:r0:', 'radix-:r2:'].forEach(id => {
-  const btn = document.querySelector(`button#${id.replace(/:/g, '\\:')}`);
-  if (btn) btn.remove();
-});
+  ['radix-:r0:', 'radix-:r2:'].forEach(id => {
+    const btn = document.querySelector(`button#${id.replace(/:/g, '\\:')}`);
+    if (btn) btn.remove();
+  });
+})();
